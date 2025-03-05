39afbdd67946663cfefd5b0415547aca29d578ef
skysw <skwa00001@stud.uni-saarland.de>
2025-03-05 19:09:14 +0100

    Add file utils to make life easier.

---
+++ src/utils.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <filesystem>
+#include <config.h>
+
+namespace sle {
+
+namespace fs = std::filesystem;
+
+// ------------ file path handling --------------
+/**
+ * @brief Fetches the filename from assets/textures/map.
+ * @param fileName The file to fetch the path of
+ * @param fileExtension The extension of the file to fetch. Defaults to .png. Do not pass '.' here.
+*/
+inline fs::path fetchTileTexture(const std::string &fileName, const std::string &fileExtension = "png") {
+    return ASSETS_PATH / "textures/map" / (fileName + "." + fileExtension);
+}
+
+/**
+ * @brief Fetches the filename from assets/map-files.
+ * @param fileName The file to fetch the path of
+ * @param fileExtension The extension of the file to fetch. Defaults to .slemap. Do not pass '.' here.
+*/
+inline fs::path fetchMapFile(const std::string &fileName, const std::string &fileExtension = "slemap") {
+    fs::path assetsDir = ASSETS_PATH / "map-files" / (fileName + "." + fileExtension);
+}
+
+}

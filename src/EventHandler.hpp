171e9545808f966049ef6430a8ad4288896c93f9
sleeepyskies <skyswalla@gmail.com>
2025-03-07 14:23:36 +0100

    Move some attributes from GameData to EventHandler

--- src/EventHandler.hpp
+++ src/EventHandler.hpp
@@ -9,9 +9,17 @@
 namespace sle {
 class EventHandler {
 private:
+    /// @brief The current mouse position in screen coordinates.
+    glm::ivec2 m_mousePos;
+    /// @brief A map of all keys, with information whether they are currently pressed.
+    std::unordered_map<SDL_Keycode, bool> m_keys;
+
     void handleMouseMotion(const SDL_Event &event, GameData &gameData);
 
 public:
+    EventHandler(): m_mousePos(glm::ivec2{0, 0}) {}
+    ~EventHandler() = default;
+
     void input(GameData &gameData);
 };
 

9de3a9c17a9672740ea8ad723b545dda94ce7791
sleeepyskies <skyswalla@gmail.com>
2025-03-07 14:23:11 +0100

    add color type def

--- src/core.hpp
+++ src/core.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <glm/vec4.hpp>
 
 namespace sle {
 /// @brief A shared pointer for a given type.
@@ -11,4 +12,8 @@ template <typename T> using cref = std::shared_ptr<const T>;
 
 /// @brief A weak pointer for a given type.
 template <typename T> using wref = std::weak_ptr<T>;
+
+/// @brief An RGBA color represented as a 4D vector
+using Color = glm::u8vec4;
+
 } // namespace sle

196f4e726d30cc6566fa85da0dd68371e1ea0c2b
skysw <skwa00001@stud.uni-saarland.de>
2025-03-08 18:16:54 +0100

    big refactor to file struture

---
+++ src/util/constants.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Constants.hpp"
+#include <glm/vec2.hpp>
+#include <string>
+
+namespace sle {
+
+const std::string WINDOW_NAME = "sle";
+constexpr int WIDTH           = 1600;
+constexpr int HEIGHT          = 850;
+
+constexpr int TILE_HEIGHT           = 150;
+constexpr int TILE_WIDTH            = 150;
+constexpr float HALF_TILE_HEIGHT    = TILE_HEIGHT / 2.f;
+constexpr float HALF_TILE_WIDTH     = TILE_WIDTH / 2.f;
+constexpr float QUARTER_TILE_HEIGHT = TILE_HEIGHT / 4.f;
+constexpr float QUARTER_TILE_WIDTH  = TILE_WIDTH / 4.f;
+
+constexpr int CHUNK_SIZE = 16;
+constexpr int CHUNK_TILE_COUNT = CHUNK_SIZE * CHUNK_SIZE;
+
+constexpr glm::ivec2 HIGHLIGHT_TILE{ 0, -10 };
+constexpr glm::ivec2 UN_HIGHLIGHT_TILE{ 0, 10 };
+
+constexpr float CAMERA_SPEED = 10.f;
+
+const std::string DEFAULT_MAP = "default";
+
+} // namespace sle
